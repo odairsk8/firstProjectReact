16749bd385ae1e95818a123b365e5e5375e033ae
Odair Ferreira <odairsk8@hotmail.com>
2017-03-16 17:39:00 +1000

    adding new features

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,11 +6,14 @@ var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
 var Home = require("../components/Home");
+var PrompContainer = require('../containers/PromptContainer');
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
+      <Route path='playerone' header="Player One" component={PrompContainer} />
+      <Route path='playertwo/:playerone' header="Player Two" component={PrompContainer} />
     </Route>
   </Router>
 );
