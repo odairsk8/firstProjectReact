9a60c95d18846dcc87753f4c2615584207d41c64
Odair Ferreira <odairsk8@hotmail.com>
2017-03-17 18:14:14 +1000

    adding battle

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,18 +4,17 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
-
 var Main = require('../components/Main');
 var Home = require("../components/Home");
-var PrompContainer = require('../containers/PromptContainer');
+var PromptContainer = require('../containers/PromptContainer');
 var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
-      <Route path='playerone' header="Player One" component={PrompContainer} />
-      <Route path='playertwo/:playerone' header="Player Two" component={PrompContainer} />
+      <Route path='playerOne' header='Player One' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
       <Route path='battle' component={ConfirmBattleContainer} />
     </Route>
   </Router>
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,30 +1,45 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
         contextTypes: {
                 router: React.PropTypes.object.isRequired
         },
-        getInitialState: function(){
+        getInitialState: function () {
                 console.log('getInitialState');
                 return {
-                        isLoading : true,
+                        isLoading: true,
                         playersInfo: []
                 }
         },
-        componentWillMount: function(){ console.log('componentWillMount'); },
-        componentWillReceiveProps: function(){console.log('componentWillReceiveProps');},
-        componentWillUnmount: function(){console.log('componentWillUnmount');},
-        componentDidMount: function(){
+        componentWillMount: function () { console.log('componentWillMount'); },
+        componentWillReceiveProps: function () { console.log('componentWillReceiveProps'); },
+        componentWillUnmount: function () { console.log('componentWillUnmount'); },
+        componentDidMount: function () {
                 var query = this.props.location.query;
-                // Fetch info from github then update state
-                console.log(query);
+                githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+                        .then(function (players) {
+                                this.setState({
+                                        isLoading: false,
+                                        playersInfo: [players[0], players[1]]
+                                })
+                        }.bind(this))
         },
-        render: function(){
+        handleInitiateBattle: function(){
+                this.context.router.push({
+                        pathname: 'results',
+                        state: {
+                                playersInfo: this.state.playersInfo
+                        }
+                });
+        },
+        render: function () {
                 return (
-                        <ConfirmBattle 
-                                isLoading={this.state.isLoading} 
-                                playersInfo={this.state.playersInfo} 
+                        <ConfirmBattle
+                                isLoading={this.state.isLoading}
+                                onInitiateBattle={this.handleInitiateBattle}
+                                playersInfo={this.state.playersInfo}
                         />
                 );
         }
